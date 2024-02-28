63ed83b66ce11e4710ad69f73bbbe5cd97161f8e
Sariafauzani <ajas2654@gmail.com>
2024-02-28 17:59:00 +0700

    latihan 03

---
+++ Foreach.java
@@ -0,0 +1,10 @@
+public class Foreach {
+    public static void main(String[] args) {
+        
+        int[] daftarNilai = {7, 3, 8, 2, 1};
+    
+        for (int nilai : daftarNilai) {
+            System.out.println(nilai);
+        }
+    }
+}
